3742852e5a7f9e47cee4ab34d8dc69bb91a816bb
SamuelDvn <samueldvn@gmail.com>
2016-08-20 19:02:25 -0300

    nada

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-Hello World!
+<?php
+  $var = "Hello World!";
+
+  define('CONSTANTE', $var);// escopo global
+
+  const CONSTANTE2 = $var// escopo de uma variavel comum
+
+?>
+<!DOCTYPE html>
+<html>
+  <?php
+  // include "layout/head.php";//varias vezes é possível, mas o navegador aceita só uma
+  // include_once "layout/head.php";//só inclui uma única vez, menos no prompt
+  // require "layout/head.php";//varias vezes é possível, e o navegador executa cada uma
+  require_once "layout/head.php";//só incui uma única vez, incluindo no prompt
+
+  require_once "layout/body.php"
+
+  ?>
+</html>
